60566829cc5e3ea9d8f16343998194acfd6cec8f
filippofilip95 <fpapranecx@gmail.com>
2025-12-02 15:16:47 +0100

    feat: update Heap Analytics initialization logic for enhanced configuration
    - Switched to the latest Heap initialization script with additional methods and custom configurations
    - Disabled server config fetching for improved performance during load
    - Updated tracking setup to align with the new Heap API structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,10 +95,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {/* Heap Analytics */}
-        <Script id="heap-analytics" strategy="beforeInteractive">
+        <Script id="heap-analytics" strategy="beforeInteractive" d>
           {`
-            window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement(""script"");r.type=""text/javascript"",r.async=!0,r.src=""https://cdn.heapanalytics.com/js/heap-""+e+"".js"";var a=document.getElementsByTagName(""script"")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=[""addEventProperties"",""addUserProperties"",""clearEventProperties"",""identify"",""resetIdentity"",""removeEventProperty"",""setEventProperties"",""track"",""unsetEventProperty""],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-            heap.load(""3823457192"");
+            window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
+            heap.load("3823457192");
           `}
         </Script>
 
